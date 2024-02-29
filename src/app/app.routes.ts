bf438cbaa847572c5b82c0453c875e6e8a4f2d11
David Maarek <davidmaarek55@gmail.com>
2024-02-29 21:41:34 +0100

    Emptied app.component.html except router-outlet, generate empty pages with their routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from "./pages/home/home.component";
+import { RankingComponent } from "./pages/ranking/ranking.component";
+import { PageNotFoundComponent } from "./pages/page-not-found/page-not-found.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+  },
+  {
+    path: 'ranking',
+    component: RankingComponent,
+  },
+  {
+    path: '**',
+    component: PageNotFoundComponent,
+  },
+];
