4b49317d36ec35e7fb8bd1ca261dd37077ca2d14
David Maarek <davidmaarek55@gmail.com>
2024-02-29 23:06:05 +0100

    Create cats service and its iterface

---
+++ src/app/interfaces/cats.interface.ts
@@ -0,0 +1,8 @@
+export interface CatsList {
+  images: CatImage[]
+}
+
+interface CatImage {
+  url: string;
+  id: string;
+}
---
+++ src/app/services/cats.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { Observable } from "rxjs";
+import { CatsList } from "../interfaces/cats.interface";
+import { HttpClient } from "@angular/common/http";
+
+@Injectable({
+  providedIn: 'root',
+})
+export class CatsService {
+
+  constructor(
+    private http: HttpClient,
+  ) { }
+
+  public getCats(): Observable<CatsList> {
+    return this.http.get<CatsList>('https://data.latelier.co/cats.json');
+  }
+}
