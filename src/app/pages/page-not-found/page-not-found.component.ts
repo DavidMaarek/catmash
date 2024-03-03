7c2ae071a1dcab6774357f4aabb2064b18a4df75
David Maarek <davidmaarek55@gmail.com>
2024-03-03 23:10:24 +0100

    Stylizing the 404 page

--- src/app/pages/page-not-found/page-not-found.component.ts
+++ src/app/pages/page-not-found/page-not-found.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from "../../components/header/header.component";
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-page-not-found',
   standalone: true,
-  imports: [],
+  imports: [
+    HeaderComponent,
+    RouterLink
+  ],
   templateUrl: './page-not-found.component.html',
   styleUrl: './page-not-found.component.scss'
 })
